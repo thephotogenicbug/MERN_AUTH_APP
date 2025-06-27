60fe8c370896b259655c10cc0753ee6d69309624
thephotogenicbug <navinkumarachari@gmail.com>
2025-06-27 12:53:45 +0530

    add production url to origins

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", process.env.PROD_URL];
 
 app.use(express.json());
 app.use(cookieParser());
